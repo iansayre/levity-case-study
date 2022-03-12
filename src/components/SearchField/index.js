01b41df3dde3a1a8a144797e6de58fc4ffb324a3
Ian Sayre <me@iansayre.com>
2022-03-11 16:36:30 -0800

    add search field

---
+++ src/components/SearchField/index.js
@@ -0,0 +1,35 @@
+import react from 'react';
+import PropTypes from 'prop-types';
+import { Button, Grid, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material';
+
+const SearchField = ({ handleSubmit, handleUsernameChange, username }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid container spacing={3}>
+        <Grid item xs={8}>
+          <TextField
+            id="username-textfield"
+            label="username"
+            onChange={handleUsernameChange}
+            size="normal"
+            value={username}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <Button type="submit" startIcon={<SearchIcon />}>
+            Search
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+SearchField.props = {
+  handleSubmit: PropTypes.func,
+  handleUsernameChange: PropTypes.func,
+  username: PropTypes.string,
+};
+
+export default SearchField;
